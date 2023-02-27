0e56a00f130ca28a576a3763d4ca15f4a41a4cc4
BHAVESH SINGH <95237594+Bhavesh9682@users.noreply.github.com>
2023-02-27 21:27:15 +0530

    Add files via upload

---
+++ Exp3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+class StaticDemo
+{
+public:
+    static int factorial(long int n)
+    {
+        if (n <= 1)
+        {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+    static bool isPrime(int n)
+    {
+        if (n <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+int main()
+{
+    long int n;
+    cout << "Enter the Number : ";
+    cin >> n;
+    int fact = StaticDemo::factorial(n);
+    bool prime = StaticDemo::isPrime(n);
+    cout << "Factorial of " << n << " is " << fact << endl;
+    cout << n << " is " << (prime ? "prime" : "not prime") << endl;
+    return 0;
+}
